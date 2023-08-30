66a48fd428c53c97008ad4148bebb341280a3348
Mt.Shadow <MtShadow@users.noreply.github.com>
2023-08-30 13:26:01 +0000

    fix test

--- test.rb
+++ test.rb
@@ -11,7 +11,7 @@ printer.transaction_print(CSJPOSLib::CMP_TP_TRANSACTION)
 printer.print_text("Hello, world!\n\n", CSJPOSLib::CMP_ALIGNMENT_CENTER, CSJPOSLib::CMP_FNT_DEFAULT, CSJPOSLib::CMP_TXT_1WIDTH | CSJPOSLib::CMP_TXT_1HEIGHT)
 printer.print_text("吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。\n\n", CSJPOSLib::CMP_ALIGNMENT_LEFT, CSJPOSLib::CMP_FNT_DEFAULT, CSJPOSLib::CMP_TXT_1WIDTH | CSJPOSLib::CMP_TXT_1HEIGHT)
 
-printer.print_bitmap("igyo.png", CSJPOSLib::CMP_BM_ASIS, CSJPOSLib::CMP_ALIGNMENT::CENTER)
+printer.print_bitmap(File.absolute_path("./igyo.png"), CSJPOSLib::CMP_BM_ASIS, CSJPOSLib::CMP_ALIGNMENT_CENTER)
 
 printer.cut_paper(CSJPOSLib::CMP_CUT_PARTIAL_PREFEED)
 
